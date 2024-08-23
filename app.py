813bdd0f26b20c32257a602941a77ba2f0acd906
Moazam16k <168353128+Moazam16k@users.noreply.github.com>
2024-08-24 02:20:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -128,6 +128,6 @@ def send_message():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 0.0.0))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
